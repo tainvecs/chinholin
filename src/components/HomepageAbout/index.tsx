4cf4115b93e2c48f33d012d9fc292284a6a24a7e
Chin-Ho Lin <tainvecs@gmail.com>
2022-11-11 01:07:44 +0800

    feat: fill in homepage about

--- src/components/HomepageAbout/index.tsx
+++ src/components/HomepageAbout/index.tsx
@@ -10,13 +10,20 @@ export default function HomepageAbout(): JSX.Element {
       <div className={styles.container}>
         <img
           className={styles.avatar}
+          alt={'chin-ho lin avatar'}
           src={require('@site/static/img/avatar.jpg').default}
         />
         <h1 className={styles.title}>{siteConfig.title}</h1>
-        <span className={styles.subtitle}>{siteConfig.tagline}</span>
+        <span className={styles.subtitle}>{''}</span>
         <hr className={styles.divider} />
       </div>
       <div className={styles.description}>
+        <p>Hi, I am a Data Scientist @ <a href="https://intelllex.com/" target="_blank">Intelllex</a>.</p>
+        <p>I am passionate about tech</p>
+        <p>and enjoy exploring it through doing side projects.</p>
+        <p>I document some projects on this site</p>
+        <p>and share my learning with <a href="/blog" target="_blank">blog posts</a>.</p>
+        <p>Cheers!🍻</p>
       </div>
     </section >
   );
