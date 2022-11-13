64445e623f6ec20a117ca3ccb9f12daf11df6503
Chin-Ho Lin <tainvecs@gmail.com>
2022-11-13 18:29:44 +0800

    chore: update homepage about page

--- src/components/HomepageAbout/index.tsx
+++ src/components/HomepageAbout/index.tsx
@@ -21,8 +21,8 @@ export default function HomepageAbout(): JSX.Element {
         <p>Hi, I am a Data Scientist @ <a href="https://intelllex.com/" target="_blank">Intelllex</a>.</p>
         <p>I am passionate about tech</p>
         <p>and enjoy exploring it through doing side projects.</p>
-        <p>I document some projects on this site</p>
-        <p>and share my learning with <a href="/blog" target="_blank">blog posts</a>.</p>
+        <p>I document some <a href="/projects">projects</a> on this site.</p>
+        <p>and share my learning with <a href="/blog">blog posts</a>.</p>
         <p>Cheers!🍻</p>
       </div>
     </section >
